c6c00c948f922c39296ef33b864579fef2a8dac8
varun varghese <talktovarun@gmail.com>
2018-10-10 20:07:50 -0500

    add place holders

---
+++ src/main/java/recursion/problems/io/karel/Karel.java
@@ -0,0 +1,28 @@
+package recursion.problems.io.karel;
+
+public class Karel {
+	
+	/*
+	 * 5  .  .  .  .  .
+	 * 
+	 * 4  .  .  .  .  .
+	 * 
+	 * 3  .  .  .  .  .
+	 * 
+	 * 2  .  .  X  .  .
+	 * 
+	 * 1  .  .  .  .  .
+	 * 
+	 *    1  2  3  4  5
+	 * 
+	 * Suppose that Karel is sitting on the intersection of 2nd Street and 3rd Avenue
+	 * Write a recursive function that returns the number of paths to get back to 
+	 * origin from the specified starting position. No unnecessary steps are taken. 
+	 * Handle 0 edge cases.
+	 */
+	public int numPathsHome(int street, int avenue) {
+		// TODO
+		return 0;
+	}
+
+}
---
+++ src/main/java/recursion/problems/io/subsequence/Subsequence.java
@@ -0,0 +1,10 @@
+package recursion.problems.io.subsequence;
+
+public class Subsequence {
+	
+	boolean hasSubsequence(String text, String subseq) {
+		// TODO
+		return false;
+	}
+
+}
