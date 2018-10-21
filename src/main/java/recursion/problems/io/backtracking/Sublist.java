62981642cc818d312d7ac2ed948e134c910b32be
varun varghese <talktovarun@gmail.com>
2018-10-21 06:57:36 -0500

    sublist initial ver

---
+++ src/main/java/recursion/problems/io/backtracking/Sublist.java
@@ -0,0 +1,21 @@
+package recursion.problems.io.backtracking;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class Sublist {
+	
+	public void sublist(List<String> input) {
+		//
+	}
+	
+	public void sublistHelper() {
+		//
+	}
+
+}
---
+++ src/test/java/recursion/problems/io/backtracking/SublistTest.java
@@ -0,0 +1,20 @@
+package recursion.problems.io.backtracking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SublistTest {
+	
+	@Test
+	public void test() {
+		log.info("Test sublist");
+	}
+
+}
