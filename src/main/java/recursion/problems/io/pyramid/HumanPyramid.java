e11b81192727ac618998d4f664414526912bff69
varun varghese <talktovarun@gmail.com>
2018-10-21 06:56:59 -0500

    human pyramid

--- src/main/java/recursion/problems/io/pyramid/HumanPyramid.java
+++ src/main/java/recursion/problems/io/pyramid/HumanPyramid.java
@@ -1,9 +1,24 @@
 package recursion.problems.io.pyramid;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HumanPyramid {
+	private static double WT_ONE = 200d;
 
 	public double weightOnBackOf(int row, int col) {
-		// TODO
-		return 0d;
+		return weightOnBackHelper(row, col);
+	}
+	
+	private double weightOnBackHelper(int row, int col) {
+		if (row == 0 && col == 0) {
+			return 0;
+		} else if (row > 0 && col == 0) {
+			return WT_ONE/2 + weightOnBackHelper(row - 1, col)/2;
+		} else if (row > 0 && col > 0 && row == col) {
+			return WT_ONE/2 + weightOnBackHelper(row - 1, col - 1)/2;
+		} else {
+			return WT_ONE + weightOnBackHelper(row - 1, col - 1)/2 + weightOnBackHelper(row - 1, col)/2;
+		}
 	}
 }
---
+++ src/test/java/recursion/problems/io/pyramid/HumanPyramidTest.java
@@ -0,0 +1,41 @@
+package recursion.problems.io.pyramid;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import lombok.extern.slf4j.Slf4j;
+import recursion.problems.io.backtracking.SublistTest;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HumanPyramidTest {
+	
+	@Autowired
+	private HumanPyramid humanPyramid;
+
+	@Test
+	public void test() {
+		assertEquals(0.0d, humanPyramid.weightOnBackOf(0, 0), 0.0d);
+		assertEquals(100.0d, humanPyramid.weightOnBackOf(1, 0), 0.0d);
+		assertEquals(100.0d, humanPyramid.weightOnBackOf(1, 1), 0.0d);
+		assertEquals(150.0d, humanPyramid.weightOnBackOf(2, 0), 0.0d);
+		assertEquals(300.0d, humanPyramid.weightOnBackOf(2, 1), 0.0d);
+		assertEquals(150.0d, humanPyramid.weightOnBackOf(2, 2), 0.0d);
+		assertEquals(175.0d, humanPyramid.weightOnBackOf(3, 0), 0.0d);
+		assertEquals(425.0d, humanPyramid.weightOnBackOf(3, 1), 0.0d);
+		assertEquals(425.0d, humanPyramid.weightOnBackOf(3, 2), 0.0d);
+		assertEquals(175.0d, humanPyramid.weightOnBackOf(3, 3), 0.0d);
+		assertEquals(187.5d, humanPyramid.weightOnBackOf(4, 0), 0.0d);
+		assertEquals(500.0d, humanPyramid.weightOnBackOf(4, 1), 0.0d);
+		assertEquals(625.0d, humanPyramid.weightOnBackOf(4, 2), 0.0d);
+		assertEquals(500.0d, humanPyramid.weightOnBackOf(4, 3), 0.0d);
+		assertEquals(187.5d, humanPyramid.weightOnBackOf(4, 4), 0.0d);
+	}
+
+}
